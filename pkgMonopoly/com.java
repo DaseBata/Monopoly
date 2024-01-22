d94d18dfdb0ae10af4c88cbb038cb08ae8be8aca
gokui9k2 <150793445+gokui9k2@users.noreply.github.com>
2024-01-22 16:08:56 -0400

    Add files via upload

---
+++ pkgMonopoly/com.java
@@ -0,0 +1,14 @@
+    public void action1 (Joueur joueur, String typeCarte, int index, ArrayList<Joueur> listeJoueur) {
+        if (typeCarte.equals("communaute")) {
+            switch (index) {
+                case 0:
+                    AllerPrison(index,joueur.caseActuelle);
+                    break;
+                case 1:
+                    
+
+                
+                    break;
+            }
+        }
+    }
